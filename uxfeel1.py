a54fadf7dbf59657ee10e7ef4fd4b43ea4c2b485
Safia <88679204+SAMsdresearch@users.noreply.github.com>
2025-05-05 10:27:48 +0300

    Update uxfeel1.py

--- uxfeel1.py
+++ uxfeel1.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 st.title("Physician Clinic Sentiment Analysis Dashboard")
 
 # Load data from Excel file
-df = pd.read_excel('result_uxfeel.xlsx', parse_dates=['Date'])
+df = pd.read_excel('result_uxfeel.xlsx')
 
 
 # Dropdown for Clinic
